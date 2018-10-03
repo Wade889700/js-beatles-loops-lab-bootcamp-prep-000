9b381eeef300894e4871fcf6e64dc4aaf2f4bbcc
severus889700 <severus889700@gmail.com>
2018-10-03 06:32:52 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // add solution here
+function theBeatlesPlay(musicians, instruments){
+  var myArray = [];
+  var b = musicians.length
+  for (var i = 0; i<b; i++){
+    var a = musicians[i] + " plays " + instruments[i]
+    myArray.push(a)
+  }
+      return myArray
+}
+
+function johnLennonFacts(facts){
+  var i = 0;
+  while(i<facts.length){
+    facts[i] = facts[i]+"!!!";
+    i++;
+  }
+  return ;
+}
+
+
+/*function iLoveTheBeatles(number){
+    do{
+    var myArray = [];
+    var i = 0; 
+    myArray.push("I love the Beatles!")
+    i++
+    }while(number>i);
+}*/
