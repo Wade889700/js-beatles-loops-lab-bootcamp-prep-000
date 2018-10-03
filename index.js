c9868f4ce9cccc635b9322e177b8100d8200f800
severus889700 <severus889700@gmail.com>
2018-10-03 06:42:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,8 @@ function johnLennonFacts(facts){
 
 
 function iLoveTheBeatles(number){
+  var myArray = [];
     do{
-    var myArray = [];
     myArray.push("I love the Beatles!");
     number++;
     }while(number<15);
